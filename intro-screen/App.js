d8fb7742bab6b1a16a5eb0f28dfaa42875ce8b5a
emerald-lan <emeraldlan1105@gmail.com>
2022-02-10 22:59:57 +0700

    added

---
+++ intro-screen/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Onboard from './screen/Onboard';
+import {View, Text, Image, StyleSheet, StatusBar, Dimensions} from 'react-native';
+const App = () => {
+  return <Onboard />
+};
+
+export default App;
