f461a4e15d20ccfcee36358e773aa6f6da2a3e00
Thế Vĩ <88361806+TheViHuynh@users.noreply.github.com>
2022-03-09 19:13:37 +0700

    update change avatar modal

--- src/screens/profile/components/EditProfile.tsx
+++ src/screens/profile/components/EditProfile.tsx
@@ -93,26 +93,30 @@ const EditProfile = (props: EditProfileProps) => {
             >
                 <View style={{flex: 1, paddingBottom: scaleSize(33), justifyContent: "flex-end", alignItems: "center", alignSelf: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
                     <View style={styles.modalView}>
-                        <Pressable
-                            onPress={openCamera}
-                            style={{marginTop: scaleSize(14)}}
-                        >
-                            <Text style={{fontSize: scaleSize(23), color: '#1D325E'}}>Take Photo</Text>
-                        </Pressable>
-                        <View style={{backgroundColor: '#BABCC1', height: scaleSize(1), width: scaleSize(376)}}/>
-                        <Pressable
-                            onPress={openLibrary}
-                            style={{marginTop: scaleSize(10)}}
-                        >
-                            <Text style={{fontSize: scaleSize(23), color: '#1D325E'}}>Choose From Library</Text>
-                        </Pressable>
-                        <View style={{backgroundColor: '#BABCC1', height: scaleSize(1), width: scaleSize(376)}}/>
+                        <View style={styles.takePhotoModalOption}>
+                            <Pressable
+                                onPress={openCamera}
+                                style={{marginTop: scaleSize(14)}}
+                            >
+                                <Text style={{fontSize: scaleSize(23), color: '#1D325E'}}>Take Photo</Text>
+                            </Pressable>
+                            <View style={{backgroundColor: '#BABCC1', height: scaleSize(1), width: scaleSize(376)}}/>
+                            <Pressable
+                                onPress={openLibrary}
+                                style={{marginTop: scaleSize(10)}}
+                            >
+                                <Text style={{fontSize: scaleSize(23), color: '#1D325E', marginBottom: scaleSize(19)}}>Choose From Library</Text>
+                            </Pressable>
+                        </View>
+                        
+                        <View style={styles.chooseLibraryModalOption}>
                         <Pressable
                             onPress={() => setChangeAvatarModalVisible(!changeAvatarModalVisible)}
                             style={{marginBottom: scaleSize(14)}}
                         >
-                            <Text style={{fontSize: scaleSize(23), color: '#1D325E', fontWeight: 'bold'}}>Cancel</Text>
+                            <Text style={{fontSize: scaleSize(23), color: '#1D325E', fontWeight: 'bold', marginTop: scaleSize(14)}}>Cancel</Text>
                         </Pressable>
+                        </View>
                     </View>
                 </View>
             </Modal>
@@ -243,11 +247,30 @@ const styles = StyleSheet.create({
     },
     modalView: {
         ...STYLES.deepShadow, 
-        height: scaleSize(183), 
+        height: scaleSize(184), 
         width: scaleSize(376), 
         alignSelf: 'center', 
         alignItems: 'center', 
         justifyContent: 'space-between',
+        //backgroundColor: '#F5F9FD',
+        borderRadius: 10
+    },
+    takePhotoModalOption: {
+        ...STYLES.deepShadow, 
+        height: scaleSize(112), 
+        width: scaleSize(376), 
+        alignSelf: 'center', 
+        alignItems: 'center', 
+        justifyContent: 'space-between',
+        backgroundColor: '#F5F9FD',
+        borderRadius: 10
+    },
+    chooseLibraryModalOption: {
+        ...STYLES.deepShadow, 
+        height: scaleSize(56), 
+        width: scaleSize(376), 
+        alignSelf: 'center', 
+        alignItems: 'center',
         backgroundColor: '#F5F9FD',
         borderRadius: 10
     }
